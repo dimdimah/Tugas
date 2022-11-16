58172ac4a18c5185b360364b97d9bf24762d197f
Dimah015 <dimaharrohman@gmail.com>
2022-11-16 20:42:21 +0700

    nambahin rumus cari gaji total, line: 28 , 29, 30 benerin rumus lembur line: 23, 24, 25

--- prak5tgs2.py
+++ prak5tgs2.py
@@ -7,9 +7,9 @@ gapok_b = 700000
 gapok_c = 900000
 
 # rumus menentukan tunjangan karyawan
-upah_kotor_A = 500000 + (500000 * 10) / 100
-upah_kotor_B = 700000 + (700000 * 15) / 100
-upah_kotor_C = 900000 + (900000 * 20) / 100
+tunjangan_A = 500000 + (500000 * 10) / 100
+tunjangan_B = 700000 + (700000 * 15) / 100
+tunjangan_C = 900000 + (900000 * 20) / 100
 
 # input data karyawan pt. xyz
 print("PROGRAM PERHITUNGAN GAJI KARYAWAN")
@@ -19,41 +19,59 @@ nama_295 = (input("Tulis Nama Anda = "))
 golongan_295 = (input("Masukan Golongan kerja anda = "))
 ttl_jamKerja_295 = int(input("total jam kerja anda selama 1 bulan = "))
 
-lembur_a = ttl_jamKerja_295 * 5000
-lembur_b = ttl_jamKerja_295 * 7500
-lembur_c = ttl_jamKerja_295 * 10000
+# Rumus cari lembur
+lembur_a = (ttl_jamKerja_295 - 200) * 5000
+lembur_b = (ttl_jamKerja_295 - 200) * 7500
+lembur_c = (ttl_jamKerja_295 - 200) * 10000
+
+# rumus cari gaji bersih/gaji total
+gatot_295_a = gapok_a + tunjangan_A + lembur_a
+gatot_295_b = gapok_a + tunjangan_A + lembur_a
+gatot_295_c = gapok_a + tunjangan_A + lembur_a
+
 print("==========================================================")
 print("================Hasil Perhitungan gaji anda===============")
 
+# perhitungan lembur karyawan /jam
 if golongan_295 == 'a':
     print()
     if ttl_jamKerja_295 >= 200:
-        x.field_names = ["Golongan", "Gaji Pokok", "Tunjangan", "Upah Lembur"]
-        x.add_row([golongan_295, "Rp 500.000", upah_kotor_A, lembur_a])
+        x.field_names = ["Golongan", "Gaji Pokok",
+                         "Tunjangan", "Upah Lembur", "Gaji Total"]
+        x.add_row([golongan_295, "Rp 500.000",
+                  tunjangan_A, lembur_a, gatot_295_a])
         print(x)
     else:
-        x.field_names = ["Golongan", "Gaji Pokok", "Tunjangan", "Upah Lembur"]
-        x.add_row([golongan_295, "Rp 500.000", upah_kotor_A, "Tidak Ada"])
+        x.field_names = ["Golongan", "Gaji Pokok",
+                         "Tunjangan", "Upah Lembur", "Gaji Total"]
+        x.add_row([golongan_295, "Rp 500.000",
+                  tunjangan_A, "Tidak Ada", gatot_295_a])
         print(x)
 elif golongan_295 == 'b':
     print()
     if ttl_jamKerja_295 >= 200:
-        x.field_names = ["Golongan", "Gaji Pokok", "Tunjangan", "Upah Lembur"]
-        x.add_row([golongan_295, "Rp 700.000", upah_kotor_B, lembur_b])
+        x.field_names = ["Golongan", "Gaji Pokok",
+                         "Tunjangan", "Upah Lembur", "Gaji Total"]
+        x.add_row([golongan_295, "Rp 700.000",
+                  tunjangan_B, lembur_b, gatot_295_b])
         print(x)
     else:
-        x.field_names = ["Golongan", "Gaji Pokok", "Tunjangan", "Upah Lembur"]
-        x.add_row([golongan_295, "Rp 700.000", upah_kotor_B, "Tidak Ada"])
+        x.field_names = ["Golongan", "Gaji Pokok",
+                         "Tunjangan", "Upah Lembur", "Gaji Total"]
+        x.add_row([golongan_295, "Rp 700.000",
+                  tunjangan_B, "Tidak Ada", gatot_295_b])
         print(x)
-
-# perhitungan lembur karyawan /jam
 elif golongan_295 == 'c':
     print()
     if ttl_jamKerja_295 >= 200:
-        x.field_names = ["Golongan", "Gaji Pokok", "Tunjangan", "Upah Lembur"]
-        x.add_row([golongan_295, "Rp 900.000", upah_kotor_C, lembur_c])
+        x.field_names = ["Golongan", "Gaji Pokok",
+                         "Tunjangan", "Upah Lembur", "Gaji Total"]
+        x.add_row([golongan_295, "Rp 900.000",
+                  tunjangan_C, lembur_c, gatot_295_c])
         print(x)
     else:
-        x.field_names = ["Golongan", "Gaji Pokok", "Tunjangan", "Upah Lembur"]
-        x.add_row([golongan_295, "Rp 900.000", upah_kotor_C, "Tidak Ada"])
+        x.field_names = ["Golongan", "Gaji Pokok",
+                         "Tunjangan", "Upah Lembur", "Gaji Total"]
+        x.add_row([golongan_295, "Rp 900.000",
+                  tunjangan_C, "Tidak Ada", gatot_295_c])
         print(x)
